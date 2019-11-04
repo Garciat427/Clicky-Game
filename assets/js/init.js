6d366cc30645a9b3e5446c90c6784b8da05997ce
Troy Garcia <tg1212@outlook.com>
2019-11-04 13:26:09 -0500

    Updates

---
+++ assets/js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.materialboxed').materialbox();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy();
+
+    
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
---
+++ precache-manifest.fc9739b2c9b9235e8a299c3323a9ad2d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81728dcd63b9c3f477504b3d32faf28f",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "3476cece31714531c15b",
+    "url": "/Clicky-Game/static/js/2.166e79ae.chunk.js"
+  },
+  {
+    "revision": "a432aeda6db3681e5afe",
+    "url": "/Clicky-Game/static/js/main.2ed9f33d.chunk.js"
+  },
+  {
+    "revision": "5e71aeb62a28ed1c3b5e",
+    "url": "/Clicky-Game/static/js/runtime-main.09783825.js"
+  }
+]);
